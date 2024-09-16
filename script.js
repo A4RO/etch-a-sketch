8acd19bcd36580fc5327aa1239efbafb6aba6cbe
Aaro Heikkinen <heikkinenaaro@gmail.com>
2024-09-16 15:13:22 +0300

    Delete useless if statement

--- script.js
+++ script.js
@@ -5,12 +5,6 @@ for(let i = 0; i < 16; i++) {
         newDiv = document.createElement("div");    
         newDiv.setAttribute("class", "grid-box"); 
         gridContainer.appendChild(newDiv);
-        if(i % 2 === 0) {
-            newDiv.style = "background-color: red";
-        }
-        else {
-            newDiv.style = "background-color: blue";
-        }
         //Box width and height should be container's size divided by amount of boxes
         newDiv.style = `width: ${700/16}px; height: ${700/16}px; background-color: red;`
     }
