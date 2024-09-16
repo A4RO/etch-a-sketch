622225f814fc08a05b133ed4967750af8feb18f3
Aaro Heikkinen <heikkinenaaro@gmail.com>
2024-09-16 15:44:44 +0300

    Add a button for creating a new grid

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 gridContainer = document.querySelector("#grid-container");
 
-for(let i = 0; i < 16; i++) {
-    for(let j = 0; j < 16; j++) {
-        newDiv = document.createElement("div");    
-        newDiv.setAttribute("class", "grid-box"); 
-        gridContainer.appendChild(newDiv);
-        //Box width and height should be container's size divided by amount of boxes
-        newDiv.style = `width: ${700/16}px; height: ${700/16}px; background-color: red;`
-        newDiv.addEventListener("mouseover", function(e) {
-            e.target.style.backgroundColor = "blue";
-        }) 
+createGrid(16);
+
+btn = document.querySelector("button");
+btn.addEventListener("click", function() {
+    let size = 0;
+    while(size > 100 || size <= 0) {
+        size = prompt("Enter number of squares per side");
+    }
+    gridContainer.remove();
+    body = document.querySelector("body");
+    gridContainer = document.createElement("div");
+    gridContainer.setAttribute("id", "grid-container");
+    body.appendChild(gridContainer);
+    createGrid(size);
+});
+
+function createGrid(size) {
+    for(let i = 0; i < size; i++) {
+        for(let j = 0; j < size; j++) {
+            newDiv = document.createElement("div");    
+            newDiv.setAttribute("class", "grid-box"); 
+            gridContainer.appendChild(newDiv);
+            //Box width and height should be container's size divided by amount of boxes
+            newDiv.style = `width: ${700/size}px; height: ${700/size}px; background-color: red;`
+            newDiv.addEventListener("mouseover", function(e) {
+                e.target.style.backgroundColor = "blue";
+            }) 
+        }
     }
 }
